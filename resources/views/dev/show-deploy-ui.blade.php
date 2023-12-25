66fca4fe2d24539db01de996fd5d775671f84984
ShineShineDev <aungshine194@gmail.com>
2023-12-25 18:53:41 +0630

    Auto commit message on 2023-12-25 18:53:41

--- resources/views/dev/show-deploy-ui.blade.php
+++ resources/views/dev/show-deploy-ui.blade.php
@@ -24,6 +24,7 @@
                     <div class="col-md-6 mt-3">
                         ....
                     </div>
+
                     <div class="mt-3">
                         <a @click="deploy" class="btn btn-secondary btn-sm">
                             <i class="bi bi-rocket"></i> Deploy Now
@@ -100,83 +101,77 @@
 @push('child-css')
     <style>
         .loader {
-            width: 32px;
-            height: 90px;
+            animation: rotate 1s infinite;
+            height: 50px;
+            width: 50px;
+        }
+
+        .loader:before,
+        .loader:after {
+            content: "";
             display: block;
-            margin: 20px auto;
-            position: relative;
-            border-radius: 50% 50% 0 0;
-            border-bottom: 10px solid #FF3D00;
-            background-color: #FFF;
-            background-image: radial-gradient(ellipse at center, #FFF 34%, #FF3D00 35%, #FF3D00 54%, #FFF 55%), linear-gradient(#FF3D00 10px, transparent 0);
-            background-size: 28px 28px;
-            background-position: center 20px, center 2px;
-            background-repeat: no-repeat;
-            box-sizing: border-box;
-            animation: animloaderBack 1s linear infinite alternate;
+            height: 20px;
+            width: 20px;
         }
 
-        .loader::before {
-            content: '';
-            box-sizing: border-box;
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%);
-            width: 64px;
-            height: 44px;
-            border-radius: 50%;
-            box-shadow: 0px 15px #FF3D00 inset;
-            top: 67px;
+        .loader:before {
+            animation: box1 1s infinite;
+            background-color: #fff;
+            box-shadow: 30px 0 0 #ff3d00;
+            margin-bottom: 10px;
         }
 
-        .loader::after {
-            content: '';
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%) rotate(45deg);
-            width: 34px;
-            height: 34px;
-            top: 112%;
-            background: radial-gradient(ellipse at center, #ffdf00 8%, rgba(249, 62, 0, 0.6) 24%, rgba(0, 0, 0, 0) 100%);
-            border-radius: 50% 50% 0;
-            background-repeat: no-repeat;
-            background-position: -44px -44px;
-            background-size: 100px 100px;
-            box-shadow: 4px 4px 12px 0px rgba(255, 61, 0, 0.5);
-            box-sizing: border-box;
-            animation: animloader 1s linear infinite alternate;
+        .loader:after {
+            animation: box2 1s infinite;
+            background-color: #ff3d00;
+            box-shadow: 30px 0 0 #fff;
         }
 
-        @keyframes animloaderBack {
-
-            0%,
-            30%,
-            70% {
-                transform: translateY(0px);
-            }
-
-            20%,
-            40%,
-            100% {
-                transform: translateY(-5px);
-            }
-        }
-
-        @keyframes animloader {
+        @keyframes rotate {
             0% {
-                box-shadow: 4px 4px 12px 2px rgba(255, 61, 0, 0.75);
-                width: 34px;
-                height: 34px;
-                background-position: -44px -44px;
-                background-size: 100px 100px;
+                transform: rotate(0deg) scale(0.8)
+            }
+
+            50% {
+                transform: rotate(360deg) scale(1.2)
             }
 
             100% {
-                box-shadow: 2px 2px 8px 0px rgba(255, 61, 0, 0.5);
-                width: 30px;
-                height: 28px;
-                background-position: -36px -36px;
-                background-size: 80px 80px;
+                transform: rotate(720deg) scale(0.8)
+            }
+        }
+
+        @keyframes box1 {
+            0% {
+                box-shadow: 30px 0 0 #ff3d00;
+            }
+
+            50% {
+                box-shadow: 0 0 0 #ff3d00;
+                margin-bottom: 0;
+                transform: translate(15px, 15px);
+            }
+
+            100% {
+                box-shadow: 30px 0 0 #ff3d00;
+                margin-bottom: 10px;
+            }
+        }
+
+        @keyframes box2 {
+            0% {
+                box-shadow: 30px 0 0 #fff;
+            }
+
+            50% {
+                box-shadow: 0 0 0 #fff;
+                margin-top: -20px;
+                transform: translate(15px, 15px);
+            }
+
+            100% {
+                box-shadow: 30px 0 0 #fff;
+                margin-top: 0;
             }
         }
     </style>

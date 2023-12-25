53d2b367a0c51474ac01c6092fda583b34157e98
ShineShineDev <aungshine194@gmail.com>
2023-12-25 17:39:53 +0630

    Auto commit message on 2023-12-25 17:39:53

--- resources/views/dev/show-deploy-ui.blade.php
+++ resources/views/dev/show-deploy-ui.blade.php
@@ -49,7 +49,22 @@
             },
             methods: {
                 deploy() {
-                    
+                    const {
+                        value: password
+                    } = await Swal.fire({
+                        title: "Enter your password",
+                        input: "password",
+                        inputLabel: "Password",
+                        inputPlaceholder: "Enter your password",
+                        inputAttributes: {
+                            maxlength: "10",
+                            autocapitalize: "off",
+                            autocorrect: "off"
+                        }
+                    });
+                    if (password) {
+                        Swal.fire(`Entered password: ${password}`);
+                    }
                     // if (!this.hasPermission) {
 
                     // }

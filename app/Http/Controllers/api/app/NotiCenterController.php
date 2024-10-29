6ba7c04bc1e91fbfd73b5d9fc6e13d016cf8f987
aunghp <aunghp.io@gmail.com>
2024-10-29 13:44:33 +0630

    fix noti center

--- app/Http/Controllers/api/app/NotiCenterController.php
+++ app/Http/Controllers/api/app/NotiCenterController.php
@@ -6,9 +6,11 @@ use App\Enums\NotiFor;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\api\app\PromotionAndSystemNotiRsource;
 use App\Http\Resources\api\app\ServiceRequestNotiRsource;
+use App\Models\Messaging;
 use App\Repositories\MessagingRepository;
 use App\Repositories\RequestFormRepository;
 use App\Traits\api\ApiResponser;
+use Exception;
 use Illuminate\Http\Request;
 
 class NotiCenterController extends Controller
@@ -17,11 +19,40 @@ class NotiCenterController extends Controller
 
     public function getPromotionAndSystem($user_id, Request $request)
     {
-        $message = MessagingRepository::getPromotionAndSystemNoti();
-        $notiCenter = PromotionAndSystemNotiRsource::collection($message)->groupBy('noti_for');
-        $notiCenter[NotiFor::TRANSACTION->value] = $this->getTransactionNoti($user_id);
 
-        return $this->successResponse('Promotion & System Noti', $notiCenter, 200);
+        try {
+
+            $notiCenter = [
+                'Transactions' => [],
+                'Promotions' => [],
+                'System' => [],
+            ];
+
+            $notifications = Messaging::where(['customer_id' => $user_id])
+                ->orderByDesc('created_at')
+                ->paginationQuery()
+                ->groupBy('noti_for')
+                ->toArray();
+
+            if (count($notifications) === 0) {
+                return $this->successResponse('Promotion & System Noti', $notiCenter, 200);
+            } else {
+                $notiCenter = [
+                    'Transactions' => $notifications['Transactions'] ?? [],
+                    'Promotions' => $notifications['Promotions'] ?? [],
+                    'System' => $notifications['System'] ?? [],
+                ];
+
+                return $this->successResponse('Promotion & System Noti', $notiCenter, 200);
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+        // $message = MessagingRepository::getPromotionAndSystemNoti();
+        // $notiCenter = PromotionAndSystemNotiRsource::collection($message)->groupBy('noti_for');
+        // $notiCenter[NotiFor::TRANSACTION->value] = $this->getTransactionNoti($user_id);
+
+        // return $this->successResponse('Promotion & System Noti', $notiCenter, 200);
     }
 
     private function getTransactionNoti($user_id)
